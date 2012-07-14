48b0052f4efa6fc86c7c6e51b837a1e1501acd88
Simon Chatteleyn <kilik2049@gmail.com>
2012-07-14 17:02:37 +0200

    add detection of movie choice + OO

--- movies.rb
+++ movies.rb
@@ -5,12 +5,29 @@ require 'pp'
 
 URL = "http://movietime.cc/"
 
-movies = {}
-i = 1
-getMovies = Net::HTTP.get(URI.parse(URL)).scan(/<img(.*?)>(.*?)<\/img>/m) do |movie|
-  movie = movie.match(/\(.*\)/).to_s.split(",")
-  movies[i] = movie[1]
-  i = i+1
+def get_movies()
+  movies = {}
+  i = 1
+  Net::HTTP.get(URI.parse(URL)).scan(/<img(.*?)>(.*?)<\/img>/m) do |movie|
+    movie = movie.to_s.match(/\(.*\)/).to_s.split(",")
+    movies[i] = movie[1]
+    i = i+1
+  end
+  return movies
 end
 
-puts pp(movies)
+def download(title)
+end
+
+puts movie_list = pp(get_movies())
+
+puts 'Type the number of the movie you want, or r to get 10 new movies: '
+input = gets.chomp
+
+if input == 'r'
+  puts get_movies()
+else
+	input = input.to_i
+  title = movie_list[input]
+  #download(title)
+end
