ed58248686bdbae3d8c918be6ac94df1693461da
Simon Chatteleyn <kilik2049@gmail.com>
2012-07-16 00:28:54 +0200

    created the JSON scrapping, but didn't handle the redirection. Will have to handle the multiple choice in searching. Then, torrents' list and it'd be good

--- movies.rb
+++ movies.rb
@@ -2,13 +2,15 @@
 
 require 'Net/http'
 require 'pp'
+require 'json'
 
 URL = "http://movietime.cc/"
+URL2 = "http://movies.io/m/search?utf8=%E2%9C%93&q="
 
 def get_movies()
   movies = {}
   i = 1
-  Net::HTTP.get(URI.parse(URL)).scan(/<img(.*?)>(.*?)<\/img>/m) do |movie|
+  Net::HTTP.get(URI.parse(URL)).scan(/<img(.*?)>(.*?)<\/img>/) do |movie|
     movie = movie.to_s.match(/\(.*\)/).to_s.split(",")
     movies[i] = movie[1]
     i = i+1
@@ -16,7 +18,18 @@ def get_movies()
   return movies
 end
 
-def download(title)
+def to_json(title)
+  # Parsing and scrapping the JSON attributes
+  data = Net::HTTP.get(URI.parse(URL2+title))
+  #Have to go the redirected page. Scrap the href.
+  #.match(/data-injected=".*"/).to_s
+  data.gsub!(/&amp;/, '')
+  data.gsub!(/&quot;/, '"')
+  data = data.match(/[^torrents:].*[^}]/)
+  return data
+end
+
+def download
 end
 
 puts movie_list = pp(get_movies())
@@ -28,6 +41,7 @@ if input == 'r'
   puts get_movies()
 else
 	input = input.to_i
-  title = movie_list[input]
-  #download(title)
+  title = movie_list[input].gsub(' ', '+')
+  to_json(title)
+  #JSON parsing
 end
